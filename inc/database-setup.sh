b562f313885b17189afe4fc3223147ffdb35731e
Hartmut Holzgraefe <hartmut@php.net>
2024-06-04 11:39:45 +0200

    create both old and new osm2pgsql database variants

--- inc/database-setup.sh
+++ inc/database-setup.sh
@@ -24,29 +24,31 @@ sudo -u postgres createuser -g maposmatic root
 sudo -u postgres createuser -g maposmatic vagrant
 sudo -u postgres createuser -g maposmatic www-data
 
+# set password for gis database user
+sudo --user=maposmatic psql --dbname=postgres --command="ALTER USER maposmatic WITH PASSWORD 'secret';"
 
-# creade database for osm2pgsql import
-sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic gis
+for DATABASE in gis osmcarto5
+do
+  # creade databases for osm2pgsql import
+  sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic $DATABASE
+
+  # set up PostGIS for osm2pgsql database
+  sudo --user=postgres psql --dbname=$DATABASE --command="CREATE EXTENSION postgis"
+  sudo --user=postgres psql --dbname=$DATABASE --command="ALTER TABLE geometry_columns OWNER TO maposmatic"
+  sudo --user=postgres psql --dbname=$DATABASE --command="ALTER TABLE spatial_ref_sys OWNER TO maposmatic"
+  sudo --user=postgres psql --dbname=$DATABASE --command="CREATE EXTENSION postgis_sfcgal"
+
+  # enable hstore extension
+  sudo --user=postgres psql --dbname=$DATABASE --command="CREATE EXTENSION hstore"
+
+  # set up maposmatic admin table
+  sudo --user=maposmatic psql --dbname=$DATABASE --command="CREATE TABLE maposmatic_admin (last_update timestamp)"
+  sudo --user=maposmatic psql --dbname=$DATABASE --command="INSERT INTO maposmatic_admin VALUES ('1970-01-01 00:00:00')"
+done
 
 # creade database for maposmatic django frontend
 sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic maposmatic
 
-# set up PostGIS for osm2pgsql database
-sudo --user=postgres psql --dbname=gis --command="CREATE EXTENSION postgis"
-sudo --user=postgres psql --dbname=gis --command="ALTER TABLE geometry_columns OWNER TO maposmatic"
-sudo --user=postgres psql --dbname=gis --command="ALTER TABLE spatial_ref_sys OWNER TO maposmatic"
-sudo --user=postgres psql --dbname=gis --command="CREATE EXTENSION postgis_sfcgal"
-
-# enable hstore extension
-sudo --user=postgres psql --dbname=gis --command="CREATE EXTENSION hstore"
-
 # enable dblink extension
 sudo --user=maposmatic psql --dbname=maposmatic --command="CREATE EXTENSION dblink"
-
-# set up maposmatic admin table
-sudo --user=maposmatic psql --dbname=gis --command="CREATE TABLE maposmatic_admin (last_update timestamp)"
-sudo --user=maposmatic psql --dbname=gis --command="INSERT INTO maposmatic_admin VALUES ('1970-01-01 00:00:00')"
-
-# set password for gis database user
-sudo --user=maposmatic psql --dbname=postgres --command="ALTER USER maposmatic WITH PASSWORD 'secret';"
 
