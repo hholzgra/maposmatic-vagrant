bd73dc6955699b178df244676a81c4000b7cfc91
Hartmut Holzgraefe <hartmut@php.net>
2024-06-12 00:37:38 +0200

    make waymarked import use common importdir and flat node file

--- inc/waymarked-update.sh
+++ inc/waymarked-update.sh
@@ -13,14 +13,17 @@ PROCESSES=2
 OPTS=""
 #OPTS="-S 5000"
 
+FLAT_NODE_FILE=$INSTALLDIR/import/waymarkedtrails/flat-nodes.dat
+
 echo "== Main DB Update =="
-wmt-makedb db update $OPTS || exit
+wmt-makedb $OPTS -j $(nproc) -n $FLAT_NODE_FILE db update
+$OPTS || exit
 echo
 
 for style in hiking cycling mtb riding skating slopes # running
 do
   echo "== $style DB Update =="
-  wmt-makedb $style update || exit
+  wmt-makedb -j $(nproc) -n $FLAT_NODE_FILE $style update || exit
   echo
 done
 
