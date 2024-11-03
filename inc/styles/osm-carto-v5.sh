d15fb9820d8b3af5eb0dcce30fb0657f443814c4
Hartmut Holzgraefe <hartmut@php.net>
2024-11-03 17:41:02 +0100

    upgrade from v5.8 to v5.9

--- inc/styles/osm-carto-v5.sh
+++ inc/styles/osm-carto-v5.sh
@@ -8,11 +8,10 @@ cd $STYLEDIR
 
 git clone --quiet https://github.com/gravitystorm/openstreetmap-carto.git openstreetmap-carto-v5
 cd openstreetmap-carto-v5
-git checkout --quiet v5.8.0
+git checkout --quiet v5.9.0
 
-sed '/\sname:/d' < project.mml > osm.mml
 patch -p1 < $INCDIR/styles/osm-carto.patch
-carto --quiet --api $MAPNIK_VERSION_FOR_CARTO osm.mml > osm.xml
+carto --quiet --api $MAPNIK_VERSION_FOR_CARTO project.mml > osm.xml
 php $FILEDIR/tools/postprocess-style.php osm.xml
 
 # create color-reduced variant of generated style
