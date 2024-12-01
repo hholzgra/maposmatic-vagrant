01fbbf9afc9786b79da274e16867ad6d88c39975
Hartmut Holzgraefe <hartmut@php.net>
2024-12-01 12:52:07 +0000

    merge

--- inc/overlays/openrailway.sql
+++ inc/overlays/openrailway.sql
@@ -5,4 +5,3 @@ CREATE INDEX planet_osm_hstore_polygon_signal_box
 CREATE INDEX planet_osm_hstore_polygon_railway
     ON planet_osm_hstore_polygon using GIST(way) 
  WHERE (tags->'railway' IS NOT NULL);
-
