f85ab0ba8697e5269b896e382b76cb0fa5f49bca
Hartmut Holzgraefe <hartmut@php.net>
2025-01-09 12:22:51 +0100

    prevent git clone from being done twice

--- inc/styles/osm-carto-flex.sh
+++ inc/styles/osm-carto-flex.sh
@@ -6,9 +6,14 @@
 
 cd $STYLEDIR
 
-git clone --quiet https://github.com/gravitystorm/openstreetmap-carto.git openstreetmap-carto-flex
-cd openstreetmap-carto-flex
-git checkout --quiet master
+if test -d osm2pgsql-carto-flex
+then
+  cd osm2pgsql-carto-flex
+else
+  git clone --quiet https://github.com/gravitystorm/openstreetmap-carto.git openstreetmap-carto-flex
+  cd openstreetmap-carto-flex
+  git checkout --quiet master
+fi
 
 patch -p1 < $INCDIR/styles/osm-carto-flex.patch
 carto --quiet --api $MAPNIK_VERSION_FOR_CARTO project.mml > osm.xml
