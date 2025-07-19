148ab98eaed5ef25f23296a3efbcf83463ef7f94
Hartmut Holzgraefe <hartmut@php.net>
2025-07-19 11:46:44 +0200

    slight reformatting, mostly whitespace

--- inc/install-packages/fonts.sh
+++ inc/install-packages/fonts.sh
@@ -2,5 +2,15 @@
 
 # download / install extra fonts
 banner "extra fonts"
-(cd $INCDIR/fonts; for script in *.sh; do basename $script ".sh"; ( . $script ); done )
+
+(
+    cd $INCDIR/fonts;
+    for script in *.sh
+    do
+	basename $script ".sh"
+	( . $script );
+    done
+)
+
+fc-cache -f # not needed for Mapnik, but good practice nonetheless
 
