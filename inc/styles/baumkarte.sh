40679f2548f040b8319e44b85f1ed620cc8b8d19
Hartmut Holzgraefe <hartmut@php.net>
2025-09-24 20:38:59 +0000

    make flex database name consistent

--- inc/styles/baumkarte.sh
+++ inc/styles/baumkarte.sh
@@ -18,5 +18,3 @@ patch -p1 < $INCDIR/styles/baumkarte.patch
 carto --quiet --api $MAPNIK_VERSION_FOR_CARTO project.mml > baumkarte.xml
 php $FILEDIR/tools/postprocess-style.php baumkarte.xml
 
-sudo -u maposmatic psql osmcarto_flex < ./Server_Update/tree_species.sql
-
