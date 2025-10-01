de92f28645faaf676bd6b757169b26f80cc8fb53
Hartmut Holzgraefe <hartmut@php.net>
2025-10-01 19:35:32 +0200

    rename some columns to make Belgian style work again

--- inc/styles/belgian.sh
+++ inc/styles/belgian.sh
@@ -11,6 +11,12 @@ cd openstreetmap-carto-be
 ln -s $SHAPEFILE_DIR data
 
 patch -p1 < $INCDIR/styles/belgian.patch
+
 carto --quiet --api $MAPNIK_VERSION_FOR_CARTO project.mml > belgian.xml
+
+sed -i -e "s/craft,/tags->'craft',/g" \
+    -i -e "s/industrial,/tags->'industrial',/g" \
+    belgian.xml
+
 php $FILEDIR/tools/postprocess-style.php belgian.xml
 
