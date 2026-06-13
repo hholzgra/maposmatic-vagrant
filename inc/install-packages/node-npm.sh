2e2eb37e0d1a0664cb5e4ede139153a8c461195a
Hartmut Holzgraefe <hartmut@php.net>
2026-06-13 20:24:31 +0200

    add some progress feedback output

--- inc/install-packages/node-npm.sh
+++ inc/install-packages/node-npm.sh
@@ -3,11 +3,14 @@
 # install extra npm packages
 banner "npm packages"
 
+echo "... setting up"
 ( curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash - ) > /dev/null
 
+echo "... installing nodejs package"
 sudo apt-get install --quiet=2 --assume-yes nodejs
 
 npm config set loglevel warn
 
+echo "... installing carto"
 npm install -g carto > /dev/null || exit 3
 
