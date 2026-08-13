45312770777a05b1ca4d5662efcd4dea9fe16871
Hartmut Holzgraefe <hartmut@php.net>
2026-08-13 17:13:21 +0200

    set correct database name for Baumkarte style

--- inc/styles/baumkarte.sh
+++ inc/styles/baumkarte.sh
@@ -18,6 +18,7 @@ cd baumkarte
 
 ln -s "$SHAPEFILE_DIR" data
 
+sed -i -e 's/dbname: "gis"/dbname: "osm2pgsql"/g' project.mml
 # patch -p1 < "$INCDIR"/styles/baumkarte.patch
 carto --quiet --api "$MAPNIK_VERSION_FOR_CARTO" project.mml > baumkarte.xml
 php "$FILEDIR"/tools/postprocess-style.php baumkarte.xml
